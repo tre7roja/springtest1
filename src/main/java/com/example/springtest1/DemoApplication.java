761aad17ae88ef7481d88adea16496d662da8b61
tre7roja <tre7roja@gmail.com>
2019-06-15 08:49:59 -0400

    modified:   DemoApplication.java

--- src/main/java/com/example/springtest1/DemoApplication.java
+++ src/main/java/com/example/springtest1/DemoApplication.java
@@ -2,12 +2,21 @@ package com.example.springtest1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+    	@RequestMapping("/")
+    	public String index() {
+        	return "Greetings from Spring Boot!";
+    	}
 
 }
+
